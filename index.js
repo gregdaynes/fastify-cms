e4463522b1535d93575622acb886d5119bbe5220
Gregory Daynes <email@gregdaynes.com>
2023-04-20 23:20:18 -0700

    feat: boilerplate for routes and api paw file

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ import { join } from 'desm'
 
 export default fp(async function (fastify, opts) {
   const localOpts = {
-
+    prefix: '/cms'
   }
 
-  const options = Object.create({}, localOpts, opts)
+  const options = structuredClone(Object.assign({}, localOpts, opts))
 
   // initialize database
   fastify.register(import(join(import.meta.url, 'database.js')), options)
+  fastify.register(import(join(import.meta.url, 'routes.js')), options)
 }, { name: 'fastify-cms', fastify: '^4.x' })
