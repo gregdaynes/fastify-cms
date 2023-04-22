fb221b6f0bbe043d284a77bf039f9f0e2e9fb597
Gregory Daynes <email@gregdaynes.com>
2023-04-21 18:27:58 -0700

    refactor: move database to plugins with autoload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import fp from 'fastify-plugin'
+import AutoLoad from '@fastify/autoload'
 import { join } from 'desm'
 
 export default fp(async function (fastify, opts) {
@@ -8,7 +9,11 @@ export default fp(async function (fastify, opts) {
 
   const options = structuredClone(Object.assign({}, localOpts, opts))
 
-  // initialize database
-  fastify.register(import(join(import.meta.url, 'database.js')), options)
-  fastify.register(import(join(import.meta.url, 'routes.js')), options)
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  await fastify.register(AutoLoad, {
+    dir: join(import.meta.url, 'plugins'),
+    options
+  })
 }, { name: 'fastify-cms', fastify: '^4.x' })
